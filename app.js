2a64c2b2f6644f6070f7a2d4b19bae27d0a0e134
Endi <endre1988@freemail.hu>
2023-08-18 08:57:20 +0200

    uj

--- app.js
+++ app.js
@@ -84,7 +84,9 @@ function init() {
   document.getElementById("current-0").textContent = "0";
   document.getElementById("current-1").textContent = "0";
   document.getElementById("name-0").textContent = "Player 1";
-  document.getElementById("name-1").textContent = "Player 2";
+  document.getElementById("name-1").textContent = "Player 2
+
++";
 }
 
 document.querySelector(".btn-new").addEventListener("click", init);
